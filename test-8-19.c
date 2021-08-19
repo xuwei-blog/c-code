6a1124bba113f47f470f85e822c3759df875e9f7
vigman-xxxw <1319284583@qq.com>
2021-08-19 22:47:19 +0800

    鹏哥C语言打卡

--- test-8-19.c
+++ test-8-19.c
@@ -2,6 +2,12 @@
 
 int main()
 {
-	printf("hehe\n");
+	printf("%d\n", sizeof(float));//4
+	printf("%d\n", sizeof(short));//2
+	printf("%d\n", sizeof(char));//1
+	printf("%d\n", sizeof(long long));//8
+	printf("%d\n", sizeof(double));//8
+	printf("%d\n",sizeof(int));//4
+	printf("%d\n", sizeof(long));//4
 	return 0;
 }
