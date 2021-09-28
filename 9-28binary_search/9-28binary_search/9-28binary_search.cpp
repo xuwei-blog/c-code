5ffced04fc946176265b42c3e12e58d140d94ea7
vigman-xxxw <1319284583@qq.com>
2021-09-28 22:37:59 +0800

    二分查找函数调用

---
+++ 9-28binary_search/9-28binary_search/9-28binary_search.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int binary_search(int arr[], int k, int s)
+{
+	int left = 0;
+	int right = s - 1;
+
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] < k)
+			left = mid + 1;
+		else if (arr[mid] > k)
+			right = mid - 1;
+		else
+			return mid;
+
+	}
+	return -1;
+
+}
+
+
+
+
+int main()
+{
+
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int k = 7;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+
+	int r = binary_search(arr, k, sz);
+	if (-1 == r)
+		printf("找不到\n");
+	else
+		printf("找到啦,下表是:%d\n", r);
+
+	return 0;
+}
