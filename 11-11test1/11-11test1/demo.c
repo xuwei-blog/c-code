c60ad34e60a5bbd8042f03b024882b5c5252505a
vigman-xxxw <1319284583@qq.com>
2021-11-11 12:12:05 +0800

    模拟实现memcpy

---
+++ 11-11test1/11-11test1/demo.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <assert.h>
+void* my_memcpy(void* dest, const void* src, size_t num)
+{
+	assert(dest && src);
+	char* ret = dest;
+	while (num--)
+	{
+		*(char*)dest = *(char*)src;
+		dest = (char*)dest + 1;
+		src = (char*)src + 1;
+	}
+	return ret;
+}
+
+int main()
+{
+	int arr1[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int arr2[10] = {0};
+	my_memcpy(arr2, arr1, 20);
+	
+
+
+	return 0;
+}
