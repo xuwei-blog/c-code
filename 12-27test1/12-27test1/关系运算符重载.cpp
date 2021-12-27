672a69629edf926f12f537b9658b94f47565e3fb
vigman-xxxw <1319284583@qq.com>
2021-12-27 22:25:27 +0800

    比较运算符重载

---
+++ 12-27test1/12-27test1/关系运算符重载.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class Person
+{
+public:
+	Person(string name, int age)
+	{
+		this->m_name = name;
+		this->m_age = age;
+	}
+	bool operator==(Person& p)
+	{
+		if (this->m_name == p.m_name && this->m_age == p.m_age)
+		{
+			return true;
+		}
+		return false;
+	}
+	bool operator!=(Person& p)
+	{
+		return !(this->m_name == p.m_name && this->m_age == p.m_age);
+	}
+	string m_name;
+	int m_age;
+};
+
+int main()
+{
+	Person p1("tom",18);
+	Person p2("tom",18);
+	if (p1 == p2)
+	{
+		cout << "p1==p2" << endl;
+	}
+	else
+	{
+		cout << "p1!=p2" << endl;
+	}
+	if (p1 != p2)
+	{
+		cout << "p1!=p2" << endl;
+	}
+	else
+	{
+		cout << "p1==p2" << endl;
+	}
+	return 0;
+}
